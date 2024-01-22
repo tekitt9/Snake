4c54bf53fbb8eca689e576a373233f4156538277
Lev <levlekhminka@gmail.com>
2024-01-22 11:10:17 +0100

    End of session

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -1,5 +1,7 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 public class Main {
 
 }
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -13,15 +13,11 @@ public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	Sprite sprite;
-	int fallSpeed = 0;
-	boolean isOnImg = false;
 
 	@Override
 	public void create () {
 		img = new Texture("badlogic.jpg");
 		batch = new SpriteBatch();
-		sprite = new Sprite(img, 0, 0, 256, 256);;
-		sprite.setPosition((float) Gdx.graphics.getWidth() / 2, (float) Gdx.graphics.getHeight() / 2);
 		Gdx.input.setInputProcessor(new InputProcessor() {
 			@Override
 			public boolean keyDown(int keycode) {
@@ -40,8 +36,6 @@ public class MyGdxGame extends ApplicationAdapter {
 
 			@Override
 			public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-                isOnImg = screenX >= sprite.getX() && screenX <= sprite.getX() + sprite.getWidth()
-                        && screenY <= Gdx.graphics.getHeight() - sprite.getY() && screenY >= Gdx.graphics.getHeight() - sprite.getY() - sprite.getHeight();
 				return false;
 			}
 
@@ -57,11 +51,6 @@ public class MyGdxGame extends ApplicationAdapter {
 
 			@Override
 			public boolean touchDragged(int screenX, int screenY, int pointer) {
-				System.out.printf("x:%d y:%d p:%d%n", screenX, screenY, pointer);
-				System.out.printf("xT:%f yT:%f%n", sprite.getX(), sprite.getY());
-				if (isOnImg) {
-					sprite.setPosition(screenX - sprite.getWidth() / 2, Gdx.graphics.getHeight() - sprite.getHeight() / 2 - screenY);
-				}
 				return false;
 			}
 
@@ -81,21 +70,6 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void render () {
 		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
-		if (sprite.getY() > 0) {
-			sprite.translate(0, -fallSpeed);
-		}
-		if (sprite.getY() < 0) {
-			sprite.setY(0);
-		}
-		if (sprite.getY() > Gdx.graphics.getHeight() - sprite.getHeight()) {
-			sprite.setY(Gdx.graphics.getHeight() - sprite.getHeight());
-		}
-		if (sprite.getX() < 0) {
-			sprite.setX(0);
-		}
-		if (sprite.getX() > Gdx.graphics.getWidth() - sprite.getWidth()) {
-			sprite.setX(Gdx.graphics.getWidth() - sprite.getWidth());
-		}
 		sprite.draw(batch);
 		batch.end();
 	}
